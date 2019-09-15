e703a5b8b967b0f0e086ef4f5f7b31a9ddc46433
kongsin <kongsin@ookbee.com>
2019-09-16 04:10:14 +0700

    add action button

--- app/src/main/java/com/example/gitexample/MainActivity.kt
+++ app/src/main/java/com/example/gitexample/MainActivity.kt
@@ -14,7 +14,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 
